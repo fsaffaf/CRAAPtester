7f8c69313c1793b5cbcaf30e127d68f8f0ff6fb9
fsaffaf <falak.saffaf@gmail.com>
2026-05-09 09:27:43 -0500

    Add publisherMap and FIELD_RULES constants

--- js/data.js
+++ js/data.js
@@ -1,1 +1,15 @@
+const publisherMap = {
+  science: ["Nature", "Science (AAAS)", "New England Journal of Medicine"],
+  technology: ["Wired", "MIT Technology Review", "IEEE Spectrum"],
+  social: ["Journal of Social Psychology"],
+  humanities: ["Journal of Philosophy"],
+  news: ["New York Times", "Washington Post", "BBC", "Reuters", "CNN"]
+};
 
+const FIELD_RULES = {
+  technology: 5,
+  science: 10,
+  social: 15,
+  humanities: 30,
+  news: 3
+};
