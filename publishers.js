b9fa8c1fc31401ccdf016e1c98ab0742a60121f2
fsaffaf <falak.saffaf@gmail.com>
2026-05-09 09:16:25 -0500

    Create publishers.js

---
+++ publishers.js
@@ -0,0 +1,7 @@
+const publisherMap = {
+  science: ["Nature","Science (AAAS)","NEJM"],
+  technology: ["Wired","MIT Technology Review"],
+  social: ["Journal of Social Psychology"],
+  humanities: ["Journal of Philosophy"],
+  news: ["NYT","Washington Post","BBC","Reuters","CNN"]
+};
